95ae4391eb74a6d246c0de78eb942ca512657842
SaToGo <stoledano97@gmail.com>
2023-03-07 17:40:42 +0100

    Add gameBoard place ship test
    add ship array to gameboard.
    
    _board changed to _boardHits.
    
    add new board to place ships _boardShips.
    
    add placeShip method, not yet implemented.
    
    game board now has dependency on Ship.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,22 +1,46 @@
+const Ship = require("./ship");
+
 function GameboardFactory(){
     // Properties
     _size = 10; // The size of the grid will be 10x10
-    
+    _ships = [];
+
     // board setting 10x10 matrix, with 0
     // 0 => cell is not hit.
-    // 1 => cell is hit.
-    _board = [];
-    for(let i = 0; i < 10; i++){
-        _board.push([])
-        _board[i].push(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    // 1 => cell has a Ship that is not hit.
+    // 2 => cell is hit.
+    _boardHits = [];
+    for(let y = 0; y < 10; y++){
+        _boardHits.push([])
+        _boardHits[y].push(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+
+    // This board will contain -1 if empty and
+    // the ship index on _ships array if that cell
+    // has a Ship.
+    _boardShips = [];
+    for(let y = 0; y < 10; y++){
+        _boardShips.push([])
+        _boardShips[y].push(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
     }
 
     // Geters Setters
     const getBoard = () => {
-        return _board;
+        return _boardHits;
     }
 
     // METHODS
+    const placeShip = (x, y, length, isHorizontal=true) => {
+        if(isHorizontal){ // Place Ship Horizontal
+            if( (x + length < _size) && (x > -1) ){
+
+            }
+        }else{ // Place Ship Vertical
+            if( (y + length < _size) && (y > -1) ){
+
+            }
+        }
+    }
 
     return {
         getBoard
--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -1,10 +1,25 @@
 const GameboardFactory = require("../gameboard");
+const ShipFactory = require("../ship")
 
 describe('GameBoard', () => {
 
-    let GB = GameboardFactory();
     it('board inicialization', () => {
+        let GB = GameboardFactory();
         expect(GB.getBoard()).toStrictEqual(boardInicialization);
+    })
+
+    it('place ship', () => {
+        let GB = GameboardFactory();
+
+        // Ship will be placed on 'x' from y,x=1,1 to y,x=1,5
+        // you can see boardPlaceShip below to see position in board
+        GB.placeShip(1, 1, 5, true);
+        expect(GB.getBoard()).toStrictEqual(boardPlaceShip);
+
+        // Ship will not be placed because is out of edges
+        // So we will keep the same board as before.
+        GB.placeShip(7, 7, 5, true);
+        expect(GB.getBoard()).toStrictEqual(boardPlaceShip);
     })
 })
 
@@ -13,6 +28,20 @@ let boardInicialization =
 [
  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+let boardPlaceShip = 
+[
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
